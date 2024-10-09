5e90da2c7498fa2c2784119c31dbbdf807146695
JuliaAndreyeva <blackonblackua@gmail.com>
2024-10-09 12:41:34 +0300

    add solution for lab task and tests for it

--- Lab1/App/Program.cs
+++ Lab1/App/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using App;
+
+Console.OutputEncoding = Encoding.Unicode;
+
+int n = 0, nthValue = 0;
+try
+{
+    n = IOHandler.ReadNValue();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error occured while reading file: {e.Message}");
+}
+
+try
+{
+    nthValue = BinarySequenceService.FindNthNumberWithThreeOnes(n);
+    Console.WriteLine($"The {n}-th number in the sequence is: {nthValue}");
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Error occured while solving this task: {e.Message}");
+}
+
+try
+{
+    IOHandler.WriteResult(nthValue);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error occured while writing file: {e.Message}");
+}
