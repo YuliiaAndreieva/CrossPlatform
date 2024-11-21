f62218468aac3521fff21e9b5f6011967b7cad6e
JuliaAndreyeva <blackonblackua@gmail.com>
2024-11-21 18:39:19 +0200

    modified some models

--- Lab5/App/Models/SecondLabViewModel.cs
+++ Lab5/App/Models/SecondLabViewModel.cs
@@ -3,13 +3,19 @@
 public class SecondLabViewModel
 {
     public int TotalSocks { get; set; } 
+    
     public int SupplierCount { get; set; } 
+    
     public List<SupplierModel> Suppliers { get; set; } = new();
+    
     public int? Result { get; set; } 
+    
     public string? ErrorMessage { get; set; } 
+    
     public class SupplierModel
     {
         public int Packs { get; set; }
+        
         public int Price { get; set; }
     }
 }
